b1ce505151ed8f6de023ddafdb8757ddb96c42f8
Aoi Kagase <datumoujidai@gmail.com>
2026-01-01 23:37:30 +0900

    Fix forward callback parametar.

--- source/natives_redis.cpp
+++ source/natives_redis.cpp
@@ -52,7 +52,7 @@ cell redis_connect(AMX *amx, cell *params)
 			sub->on_message([](std::string channel, std::string msg) {
 				MF_Log("[REDIS:DEBUG] ON_MESSAGE: channel='%s', message='%s'", channel.c_str(), msg.c_str());
 				// Process message of MESSAGE type.
-				MF_ExecuteForward(ForwardRedisOnMessage, channel, msg);
+				MF_ExecuteForward(ForwardRedisOnMessage, channel.c_str(), msg.c_str());
 			});
 		}
 
